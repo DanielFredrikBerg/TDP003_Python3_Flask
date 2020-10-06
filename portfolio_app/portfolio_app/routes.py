eea69cc2804b0b1c1e018c680fca9182228b44cb
danhu849 <danhu849@gitlab.liu.se>
2020-10-06 11:57:25 +0200

    added own version of api for later use

--- portfolio_app/portfolio_app/routes.py
+++ portfolio_app/portfolio_app/routes.py
@@ -1,12 +1,13 @@
 from flask import render_template
 from portfolio_app import app
+import daniel_api
 
 # /
 @app.route('/')
 @app.route('/home')
 def home():
     user= {'username': 'Daniel'}
-    return render_template('index.html', title='Home', user='Faggot')
+    return render_template('index.html', title='Home', user=user)
 
 
 # /list
