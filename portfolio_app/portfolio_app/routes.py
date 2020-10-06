bc866e8480bf88ac6cb00b615d25c53229d24799
danhu849 <danhu849@gitlab.liu.se>
2020-10-06 12:18:51 +0200

    can't import api. made jinja2 test files

--- portfolio_app/portfolio_app/routes.py
+++ portfolio_app/portfolio_app/routes.py
@@ -2,6 +2,8 @@ from flask import render_template
 from portfolio_app import app
 import daniel_api
 
+db = daniel_api.load('data.json')
+
 # /
 @app.route('/')
 @app.route('/home')
@@ -13,7 +15,7 @@ def home():
 # /list
 @app.route('/list')
 def list():
-    return "This is /list"
+    return render_template('list.html', title='Projects', db=db)
 
 
 # /project/id
