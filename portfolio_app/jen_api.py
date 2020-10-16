01eb76941ef19a500936b78283a5b86fb99cf8ec
jenoh242 <jenoh242@student.liu.se>
2020-10-16 17:55:59 +0200

    Added documentation for jen_data.py.

--- portfolio_app/jen_api.py
+++ portfolio_app/jen_api.py
@@ -4,6 +4,7 @@ import copy
 import sys
 
 def load(path):
+	"""Loads json from `path` and returns list sorted on projects ID"""
 	try:
 		with open(str(path), 'r') as data:
 			return sorted(json.load(data), key=lambda project: project['project_id'])
@@ -12,21 +13,36 @@ def load(path):
 		return None
 
 def add_project(db, project):
+	"""Adds project to `db`"""
 	db.append(project)
 
 #def delete_project(db, project):
 def write_db_to_json(db, save_file):
+	"""Writes `db` to file with path `save_file`, overwriting excisting data."""
 	with open(str(save_file), 'w') as json_file:
 		json.dump(db, json_file)
 
-def get_project_count(db): return len(db)
+def get_project_count(db):
+	"""Returns int of how many projects in `db`'"""
+	return len(db)
 
 def get_project(db, id):
+	"""Returns first `project` in `db` mathing `id`."""
 	for project in db:
 		if project['project_id'] == id:
 			return project
 
 def search(db, sort_by='start_date', sort_order='desc', techniques=None, search=None, search_fields=None):
+	"""##Search
+	Will return a list of projects in `db` mathing **all** search requirements:
+
+	1. Only match projects containing all `techniques`.
+
+	2.  Only match projects containing string `search` in atleast **one** field in `search_fields`.
+		If `search_fields` is empty, all fields will be checked.
+
+	Will sort by field `sort_by`. In ascending order if `sort_order` has value `desc`, descending if value is `asc`.
+	"""
 	projects = copy.deepcopy(db)
 	try:
 		if techniques:
@@ -46,16 +62,21 @@ def search(db, sort_by='start_date', sort_order='desc', techniques=None, search=
 		isreverse = False
 	return sorted(projects, key=lambda results: results[sort_by], reverse=isreverse)
 
-def get_technique_stats(db): return {technique: [{'id': project['project_id'], 'name': project['project_name']} for project in search(db, search=technique, search_fields=['techniques_used'])] for technique in get_techniques(db)}
+def get_technique_stats(db):
+	"""Returns a dictionary where all techniques used are the keys, and the contents are lists of dictionaries of projects IDs and names."""
+	return {technique: [{'id': project['project_id'], 'name': project['project_name']} for project in search(db, search=technique, search_fields=['techniques_used'])] for technique in get_techniques(db)}
 
 def get_search_fields(db):
+	"""Returns available fields to search in `db`."""
 	return db[0].keys()
 
-def get_techniques(db): return list(dict.fromkeys(sorted(
-	[techniques for project in db for techniques in project['techniques_used']])))
+def get_techniques(db):
+	"""Returns available techniques in `db`."""
+	return list(dict.fromkeys(sorted([techniques for project in db for techniques in project['techniques_used']])))
 
 
 def main():
+	"""Only used for testing purposes."""
 	project = {
 		"start_date": "2019-09-08",
 		"short_description": "no",
