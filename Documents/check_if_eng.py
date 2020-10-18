82a80228292467fb91146be839e1ac693435db29
danhu849 <danhu849@gitlab.liu.se>
2020-10-18 15:42:23 +0200

    unknown words in files now outputs words in .txt files

--- Documents/check_if_eng.py
+++ Documents/check_if_eng.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import re
 
 def load_wordlist(wordlist_file)-> set:
     """Makes a set of a '\n' separated wordlist file."""
@@ -9,7 +10,7 @@ def load_wordlist(wordlist_file)-> set:
     return set(wordlist)
     
 
-def if_english(word, word_list)-> bool:
+def is_english(word, word_list)-> bool:
     """Checks if word is in word_list"""
     return word in word_list
 
@@ -25,7 +26,18 @@ def files_in_dir(top_dir, end):
     return path_list
 
 
-
+def unknown_words_to_file(file_path, wordlist):
+    """Writes words from 'file_path' not in wordlist to 'file_path.txt)"""
+    unknown_words = set()
+    with open(str(file_path), 'r') as file_:
+        file_words = re.findall(r'\S+', str(file_.read()))
+        for word in file_words:
+            if not is_english(word, wordlist):
+                unknown_words.add(word)
+    if len(unknown_words) > 0:
+        with open(''.join(str(file_path), '_words.txt'), 'w') as file_:
+            file_.write(unknown_words)
+    return 0
                                  
 def main():
     w_list  = sys.argv[1]
