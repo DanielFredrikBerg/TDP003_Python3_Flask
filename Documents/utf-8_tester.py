15293491ec63799ea3df924c9afdba8c76f8e2e6
danhu849 <danhu849@gitlab.liu.se>
2020-10-21 17:43:16 +0200

    added pics and updated testdocumentation

--- Documents/utf-8_tester.py
+++ Documents/utf-8_tester.py
@@ -6,9 +6,9 @@ def is_utf8(file_name):
     """Checks if file 'file_name' has valid utf-8 encoding."""
     try:
         codecs.open(str(file_name), encoding="utf-8", errors='strict').readlines()
-        return 'valid utf-8'
+        return str(file_name) + ' is valid utf-8.'
     except UnicodeDecodeError:
-        return 'invalid utf-8'
+        return str(file_name) + ' is invalid utf-8'
 
 
 def main():
