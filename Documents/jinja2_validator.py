5e116feb1a60e91110d1d51d25019afccb1eee64
danhu849 <danhu849@gitlab.liu.se>
2020-10-21 18:52:24 +0200

    comments added to jinja2_validator.py, testdoc updated

--- Documents/jinja2_validator.py
+++ Documents/jinja2_validator.py
@@ -5,6 +5,7 @@ import os
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 def jinja2_parser(path):
+    """Checks jinja2 syntax. Returns 0 if correct. Raises error TemplateSyntaxError if incorrect."""
     env = Environment(
         loader=FileSystemLoader(str(path)),
         autoescape=select_autoescape(['html'])
@@ -12,11 +13,13 @@ def jinja2_parser(path):
     templates = [x for x in env.list_templates() if x.endswith('.html')]
     for template in templates:
         t = env.get_template(template)
+        print('Now parsing file: ' + str(template))
         env.parse(t)
     return 0
 
 def main():
-    print(jinja2_parser(sys.argv[1]))
+    if not jinja2_parser(sys.argv[1]):
+        print('Exited without errors')
 
 if __name__ == '__main__':
     main()
